9eae74151e3457c2b53baa9c440de059465f7b8e
Vitaliy Shirinkin <vitaliy.shirinkin@gmail.com>
2024-03-31 21:05:14 +0500

    Implemented test with ~20 connections

--- test.sh
+++ test.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+CLIENT_COUNT=20
+WAIT_SEC=5
+
 echo "Run tests:"
 
 PID=$(pidof ttm)
@@ -10,3 +13,24 @@ else
     echo "[PASSED] Server running"
 fi
 
+for iter in $(seq 1 $CLIENT_COUNT); do
+    pos1=$((1000+iter*10))
+    echo ".. prepare client $iter"
+    cat /var/log/syslog -n | head -n $pos1 | tail -n 1000 > "telnet$iter.txt"
+done
+
+for iter in $(seq 1 $CLIENT_COUNT); do
+    echo ".. run client $iter"
+    netcat localhost 5000 < "telnet$iter.txt" >/dev/null &
+done
+
+echo ".. waiting $WAIT_SEC seconds"
+sleep $WAIT_SEC
+
+PID=$(pidof ttm)
+if [ -z "$PID" ]; then
+    echo "[FAILED] Server 'ttm' was crashed"
+    exit 1
+else
+    echo "[PASSED] Server still running"
+fi
